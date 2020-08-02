293d6a3a6ea31ef56a1047b6c7ac3cd0c82ca170
ahstop1112 <>
2020-08-02 18:52:08 +0800

    add Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
+import HomePage from './components/HomePage';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+const App = () => {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
 }
 
 export default App;
