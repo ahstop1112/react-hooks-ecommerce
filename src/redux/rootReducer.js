facfb53a016f3b3adfa7f15ce270455bb879b484
ahstop1112 <>
2020-08-16 14:14:20 +0800

    add window storage session

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/userReducer';
 import cartReducer from './cart/cartReducer';
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
-})
-//combined all of the states together
-//user reducer
-//cart reducer
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer
+});
+
+export default persistReducer(persistConfig, rootReducer);
